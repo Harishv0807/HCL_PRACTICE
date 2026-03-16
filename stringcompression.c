c966ce8577920a8d769e82d82b58d4dd80e29568
Harishv0807 <155165145+Harishv0807@users.noreply.github.com>
2026-03-16 10:08:33 +0530

    Add files via upload

---
+++ stringcompression.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int count;
+
+    printf("Enter string: ");
+    scanf("%s",str);
+
+    int len = strlen(str);
+
+    for(int i=0;i<len;i++)
+    {
+        count=1;
+
+        while(str[i]==str[i+1])
+        {
+            count++;
+            i++;
+        }
+
+        printf("%c%d",str[i],count);
+    }
+
+    return 0;
+}
